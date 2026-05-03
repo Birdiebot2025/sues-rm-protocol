5fa42c34c0b628efa1527d07dfbea45383772855
labaicai114514 <3150836351@qq.com>
2026-05-03 20:40:21 +0800

    移除 Protocol_UpDataReferee_t 结构体中的 damage_type 字段

--- protocol.h
+++ protocol.h
@@ -98,7 +98,6 @@ struct Protocol_UpDataReferee_t {
   uint16_t team_exchange_ammo_left_17mm;  /* 队伍17mm弹丸剩余可兑换数量*/
   uint8_t current_stance;                 /* 哨兵当前姿态，1为进攻姿态，2为防御姿态，3为移动姿态 */
   bool can_activate_mechanism;           /* 己方能量机关是否能够进入正在激活状态，1 为当前可激活 */
-  uint16_t damage_type;                   /* 扣血类型，0-受到弹丸伤害 */
   bool be_attacked;                 /* 是否被攻击 1-被攻击 0-未被攻击 */
   bool road_attention;                 /* 对方起伏路段是否有关键单位，有为1,无为0 */
   uint16_t checksum = 0;
